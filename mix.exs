1a5096452310da1c64d1810063d8927c0ea7f074
Matej Minarik <mato.minarik@azet.sk>
2018-04-21 17:15:13 +0200

    Add dialyxir dependency to test environment

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule PhxRunningTracker.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:dialyxir, "~> 0.5", only: :dev, runtime: false}
+      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false}
     ]
   end
 
