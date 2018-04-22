17915f02d0c0b686f4715f00a8d2c83a71ea91cf
Matej Minarik <mato.minarik@azet.sk>
2018-04-22 07:53:46 +0200

    Render pace

--- lib/phx_running_tracker_web/views/run_log_view.ex
+++ lib/phx_running_tracker_web/views/run_log_view.ex
@@ -1,3 +1,13 @@
 defmodule PhxRunningTrackerWeb.RunLogView do
   use PhxRunningTrackerWeb, :view
+
+  def format_pace({ hours, minutes, seconds, ms }) do
+    "#{time_pad(hours)}:#{time_pad(minutes)}:#{time_pad(seconds)}"
+  end
+
+  defp time_pad(value) do
+    value
+    |> Integer.to_string
+    |> String.pad_trailing(2, "0")
+  end
 end
