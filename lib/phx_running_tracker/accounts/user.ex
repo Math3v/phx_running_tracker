91c05e2e7f18bae2fbbcdabcfc6c2b372a98b847
Matej Minarik <mato.minarik@azet.sk>
2018-06-03 13:52:54 +0200

    Implement Password Hashing

--- lib/phx_running_tracker/accounts/user.ex
+++ lib/phx_running_tracker/accounts/user.ex
@@ -1,11 +1,12 @@
 defmodule PhxRunningTracker.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
-
+  alias PhxRunningTracker.Accounts.Credential
 
   schema "users" do
     field :name, :string
     field :username, :string
+    has_one :credential, Credential
 
     timestamps()
   end
@@ -16,5 +17,11 @@ defmodule PhxRunningTracker.Accounts.User do
     |> cast(attrs, [:name, :username])
     |> validate_required([:name, :username])
     |> unique_constraint(:username)
+  end
+
+  def registration_changeset(user, attrs) do
+    user
+    |> changeset(attrs)
+    |> Ecto.Changeset.cast_assoc(:credential, with: &Credential.changeset/2)
   end
 end
--- test/phx_running_tracker/accounts/accounts_test.exs
+++ test/phx_running_tracker/accounts/accounts_test.exs
@@ -68,9 +68,9 @@ defmodule PhxRunningTracker.AccountsTest do
   describe "credentials" do
     alias PhxRunningTracker.Accounts.Credential
 
-    @valid_attrs %{email: "some email", password_hash: "some password_hash"}
-    @update_attrs %{email: "some updated email", password_hash: "some updated password_hash"}
-    @invalid_attrs %{email: nil, password_hash: nil}
+    @valid_attrs %{email: "some email", password: "some password_hash"}
+    @update_attrs %{email: "some updated email", password: "some updated password_hash"}
+    @invalid_attrs %{email: nil, password: nil}
 
     def credential_fixture(attrs \\ %{}) do
       {:ok, credential} =
@@ -81,20 +81,24 @@ defmodule PhxRunningTracker.AccountsTest do
       credential
     end
 
+    def nullify_password(credential) do
+      %{credential | password: nil}
+    end
+
     test "list_credentials/0 returns all credentials" do
       credential = credential_fixture()
-      assert Accounts.list_credentials() == [credential]
+      assert Accounts.list_credentials() == [credential |> nullify_password]
     end
 
     test "get_credential!/1 returns the credential with given id" do
       credential = credential_fixture()
-      assert Accounts.get_credential!(credential.id) == credential
+      assert Accounts.get_credential!(credential.id) == credential |> nullify_password
     end
 
     test "create_credential/1 with valid data creates a credential" do
       assert {:ok, %Credential{} = credential} = Accounts.create_credential(@valid_attrs)
       assert credential.email == "some email"
-      assert credential.password_hash == "some password_hash"
+      assert Comeonin.Bcrypt.checkpw("some password_hash", credential.password_hash) == true
     end
 
     test "create_credential/1 with invalid data returns error changeset" do
@@ -106,13 +110,13 @@ defmodule PhxRunningTracker.AccountsTest do
       assert {:ok, credential} = Accounts.update_credential(credential, @update_attrs)
       assert %Credential{} = credential
       assert credential.email == "some updated email"
-      assert credential.password_hash == "some updated password_hash"
+      assert Comeonin.Bcrypt.checkpw("some updated password_hash", credential.password_hash) == true
     end
 
     test "update_credential/2 with invalid data returns error changeset" do
       credential = credential_fixture()
       assert {:error, %Ecto.Changeset{}} = Accounts.update_credential(credential, @invalid_attrs)
-      assert credential == Accounts.get_credential!(credential.id)
+      assert credential |> nullify_password == Accounts.get_credential!(credential.id)
     end
 
     test "delete_credential/1 deletes the credential" do
